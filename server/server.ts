b5024d8a7767bc2d92b95a0b568be7fea52070e0
ledzep68 <1968zep@gmail.com>
2026-01-30 17:30:34 +0900

    2025/1/29 postgres supabase対応に伴う変更

--- server/server.ts
+++ server/server.ts
@@ -40,6 +40,14 @@ app.use((req, res, next) => {
     next();
 });
 
+//CORS設定 本番：render.com 開発：viteサーバー
+app.use(cors({
+    origin: isProduction ? process.env.FRONTEND_URL : 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']  
+}));
+
 app.use(express.json());
 
 //セッション管理
@@ -60,14 +68,6 @@ app.use(session({
     },
     proxy: true,
     name: 'sessionID'
-}));
-
-//ミドルウェア設定　CORS設定 Viteの開発サーバーとのコミュニケーション
-app.use(cors({
-    origin: isProduction ? process.env.FRONTEND_URL : 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization']  
 }));
 
 app.use('/api/auth', usersRouter);
